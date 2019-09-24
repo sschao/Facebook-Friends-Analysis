97db4479cd857473ad12fa16881c46b5bb79718c
Sichen (Shawn) Chao <shawns.chao@gmail.com>
2019-09-23 20:43:37 -0400

    Add files via upload
    Analyzes your friend's mutual friends

--- mutual_friends_analysis.py
+++ mutual_friends_analysis.py
@@ -16,10 +16,11 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
+friends_html = 'C:\\Users\\shawn\\Desktop\\Programming\\Facebook\\db\\index.html'
 usr=input('Enter Email Id:') #enter email
 pwd=input('Enter Password:') #enter password
 
-driver = webdriver.Chrome("(input your own chrome driver directory)") #change this path to appropriate chrome driver directory
+driver = webdriver.Chrome("C:\\Users\\shawn\\Desktop\\Programming\\Facebook\\chromedriver.exe") #change this path to appropriate chrome driver directory
 driver.get("http://facebook.com")
 
 username_box = driver.find_element_by_id('email')
@@ -30,10 +31,10 @@ password_box.send_keys(pwd)
 
 login_box = driver.find_element_by_id('loginbutton')
 login_box.click()
-
-actions = ActionChains(driver)
-actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 0,0)
-actions.move_by_offset(100, 200).click().perform()
+def click_random_spot():
+    actions = ActionChains(driver)
+    actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 0,0)
+    actions.move_by_offset(100, 200).click().perform()
 
 def download_friends():
     driver.get("https://m.facebook.com/me/friends")
@@ -43,11 +44,14 @@ def download_friends():
     while driver.find_elements_by_css_selector('#m_more_friends'):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(5)
+def get_soup(driver):
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    return(soup)
 
 download_friends()
+soup = get_soup(driver)
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
 
 mutual_friends = soup.findAll("div", { "class" : "notice ellipsis" })
 friend_names = soup.findAll("div", { "class" : "_84l2" })
@@ -66,12 +70,12 @@ for i in range(len(mutual_friends)):
     friend_name =  friend_names[i].find("a").text
     if friend_name in mutual_friend_dict.keys():
         dup_count = 0
-        while friend_name in mutual_friend_dict.keys():
+        while friend_name in mutual_friend_dict.keys(): #handles the duplicate error attaching an underscore and a label number to the name
             dup_count+=1
             if dup_count == 1: #first iteration so friend name does not have any extra stuff added onto it
-                friend_name = f"{friend_name}_{str(dup_count)}"
+                friend_name = friend_name+"_"+str(dup_count)
             else:
-                friend_name = f"{friend_name[:-len(str(dup_count-1))-1]}_{str(dup_count)}" #concise way to label duplicates
+                friend_name = friend_name[:-len(str(dup_count-1))-1]+"_"+str(dup_count) #concise way to label duplicates
 
     mutual_friend_dict[friend_name] = num_mutual_friends
     try:
@@ -84,7 +88,7 @@ top_mutual_friends = sorted(mutual_friend_dict, key=mutual_friend_dict.get, reve
 
 df_friends = pd.DataFrame(list(mutual_friend_dict.items()), columns=['Friend Name', 'Number of Mutual Friends'])
 df_friends_decr = df_friends.sort_values(by =["Number of Mutual Friends"], ascending = False).reset_index(drop=True)
-df_friends_decr["Ranking"] = df_friends_decr.index+1
+df_friends_decr["Ranking"] = df_friends_decr["Number of Mutual Friends"].rank(method = 'min', ascending = False)
 
 df_friends_decr["Percentile"],df_friends_decr["Facebook Link"] = [None,None]
 for index, row in df_friends_decr.iterrows(): #create percentile column
@@ -94,15 +98,14 @@ for index, row in df_friends_decr.iterrows(): #create percentile column
     except KeyError: #people who deleted their FB
         pass
 
-df_friends_decr.to_csv("(Input your own file directory)", index = False) #change this
+df_friends_decr.to_csv("C:\\Users\\shawn\\Desktop\\Programming\\Facebook\\Facebook Friends, Mutual Friends, Link.csv", index = False) #change this
 plt.figure()
 plt.plot(df_friends_decr["Percentile"], df_friends_decr["Number of Mutual Friends"])
 plt.title("Number of Facebook Friends vs Percentile")
 plt.xlabel("Percentile")
 plt.ylabel("Number of Facebook Friends")
 
-def find_friend_info(df, friend_name): #if multiple people with the same name returns both
+def find_friend_info(df, friend_name): #useful for finding specific friend info. If multiple people with the same name returns both
     df_friend= df[df["Friend Name"].str.contains(friend_name)] 
     return df_friend
-
 
